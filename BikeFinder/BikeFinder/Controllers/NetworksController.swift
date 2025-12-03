f6bffe1ae910fa1e405bcd289bdac672c5334483
Robert Smith <robert@inkrh.com>
2025-12-03 17:56:27 -0600

    Sorting networks

--- BikeFinder/BikeFinder/Controllers/NetworksController.swift
+++ BikeFinder/BikeFinder/Controllers/NetworksController.swift
@@ -14,6 +14,7 @@ class NetworksController {
             do {
                 let (data, _) = try await URLSession.shared.data(from: url)
                 let json = JSON(data)
+                print(json)
                 return await decodeReseult(json)
                 
             }
@@ -34,10 +35,12 @@ class NetworksController {
     }
     
     func decodeReseult(_ result: JSON) async -> NetworksModel {
-        let JSONDecoder = JSONDecoder()
+        let jsonDecoder = JSONDecoder()
         do {
-            let decodedData = try JSONDecoder.decode(NetworksModel.self, from: Data(try result.rawData()))
-            return decodedData
+            // decode and order by country then city
+            let decodedData = try jsonDecoder.decode(NetworksModel.self, from: Data(try result.rawData()))
+            let sortedNetworks = decodedData.networks.sorted(by: { ($0.location.country, $0.location.city) < ($1.location.country, $1.location.city) })
+            return NetworksModel(networks: sortedNetworks)
         } catch {
             print("Error decoding JSON: \(error)")
         }
